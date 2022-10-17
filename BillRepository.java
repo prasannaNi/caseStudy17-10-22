1ed7d9fc89b1fa7fcc1785dec4231361707e4aa4
prasannaNi <113326754+prasannaNi@users.noreply.github.com>
2022-10-17 20:05:35 +0530

    Add files via upload

---
+++ BillRepository.java
@@ -0,0 +1,11 @@
+package com.gl.caseStudy4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.gl.caseStudy4.bean.Bill;
+
+public interface BillRepository extends JpaRepository<Bill, Long> {
+	@Query("select max(id) from Bill")
+	public Long findMaxBillId();
+}
---
+++ MyUsersService.java
@@ -0,0 +1,28 @@
+package com.gl.caseStudy4.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.gl.caseStudy4.bean.MyUsers;
+import com.gl.caseStudy4.repository.MyUsersRepository;
+
+@Service
+public class MyUsersService implements UserDetailsService  {
+	
+	@Autowired
+	private MyUsersRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		
+		return repository.findById(username).get();
+	}
+	public void save(MyUsers users) {
+		repository.save(users);
+	}
+}
